acd5dc0753dcb612ed65806e3949e45f663fb36e
Evan Taylor <taylorevan16@gmail.com>
2025-04-28 14:01:03 -0400

    Adding the Xcode project files

---
+++ Fruit_Finder/Fruit_Finder/Fruit_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fruit_FinderApp.swift
+//  Fruit_Finder
+//
+//  Created by Evan Taylor on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Fruit_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
